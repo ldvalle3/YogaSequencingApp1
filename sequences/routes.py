d525eed11d9d8650e409f5c40f32f251b1ec20b3
ldvalle3 <127244361+ldvalle3@users.noreply.github.com>
2024-01-05 15:33:11 -0700

    added registration validation

--- sequences/routes.py
+++ sequences/routes.py
@@ -1,7 +1,8 @@
 from run import app
-from flask import render_template
-from sequences.models import Item
+from flask import render_template, redirect, url_for
+from sequences.models import Item, User
 from sequences.forms import RegisterForm
+from sequences import db
 
 
 @app.route('/')
@@ -16,7 +17,17 @@ def sequencing_page():
     return render_template('sequencing.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:  # if there are not errors from the validations
+        for err_msg in form.errors.values():
+            print(f"There was an error with creating a user: {err_msg}")
     return render_template('register.html', form=form)
