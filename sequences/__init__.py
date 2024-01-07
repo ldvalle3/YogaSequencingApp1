517510dbd67e0c2aeb58136dcf62508c3ed34ee4
ldvalle3 <127244361+ldvalle3@users.noreply.github.com>
2024-01-07 00:02:05 -0700

    added functionalities of login/register

--- sequences/__init__.py
+++ sequences/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sequencing.db'
 app.config['SECRET_KEY'] = '54a1e3f43f06dba85aa820ea'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from sequences import routes
