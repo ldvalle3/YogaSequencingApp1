7ed9f8abde5b0d5e6b8a736706b81b465be116ff
ldvalle3 <127244361+ldvalle3@users.noreply.github.com>
2024-01-08 14:16:37 -0700

    Main (#4)
    * added email/user validations
    
    * added functionalities of login/register
    
    * added modals
    
    * merged changes

--- sequences/models.py
+++ sequences/models.py
@@ -1,10 +1,5 @@
-
 from sequences import db, app, login_manager
 from sequences import bcrypt
-
-from market import db, app, login_manager
-from market import bcrypt
-
 from flask_login import UserMixin
 
 
